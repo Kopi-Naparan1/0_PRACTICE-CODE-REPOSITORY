4b92d81655e39e648590f90f6f1bbd61e00b37e0
Nyro Khufraleq <kopinaparan13@gmail.com>
2025-09-06 21:27:16 +0800

    Delete Python_1 directory

--- Python_1/README.md
+++
@@ -1,2 +0,0 @@
-# 0_PRACTICE-CODE-REPOSITORY
-All of the practice codes I have from this few months are all in here.
--- Python_1/Re-learn/Phase_2/Lesson_4.py
+++
@@ -1,6 +0,0 @@
-print("start")
-import pdb; pdb.set_trace()   # <-- stop here
-x = 10
-y = 0
-z = x / y   # <-- this will crash
-print("done")
